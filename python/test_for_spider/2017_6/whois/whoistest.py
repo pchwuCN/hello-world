327d5ee0e3027f4ca5b0c7bdec2f530574ca960a
pchwu <pchwu@grandstream.cn>
2017-06-07 16:52:25 +0800

    [NBF] change vimrc
    Cause: None
    Solution: None
    Fixed Version: 1.0.0.0
    Branch: master

--- python/test_for_spider/2017_6/whois/whoistest.py
+++ python/test_for_spider/2017_6/whois/whoistest.py
@@ -1,3 +1,10 @@
-import whois
+#########################################################################
+# File Name: whoistest.py
+# Author: pchwu
+#########################################################################
 
+#!/use/bin/python
+#coding=utf-8
+
+import whois
 print(whois.whois("appspot.com"))
