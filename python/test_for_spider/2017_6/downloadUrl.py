48b39374f2dce9f9075c19cd79f278956ee4f031
pchwu <pchwu@grandstream.cn>
2017-06-08 17:58:06 +0800

    [NBF] test linkCrawler
    Cause: None
    Solution: None
    Fixed Version: 1.0.0.0
    Branch: master

--- python/test_for_spider/2017_6/downloadUrl.py
+++ python/test_for_spider/2017_6/downloadUrl.py
@@ -2,19 +2,25 @@ import urllib2
 import re
 import time 
 
-def download(url, user_agent='wdwp', num_retry=2):
+def download(url, user_agent='wdwp', proxy=None, num_retry=2):
     print("Downloading: ", url)
     headers = {'User-agent': user_agent}
     request = urllib2.Request(url, headers=headers)
+    opener = urllib2.build_opener()
+    if proxy:
+        proxy_params = {urlparse.urlparse(url).scheme: proxy}
+        opener.add_handler(urllib2.proxyHandler(proxy_params))
     try:
-        html = urllib2.urlopen(request).read()
-        time.sleep(0.4)
+        #html = urllib2.urlopen(request).read()
+        html = opener.open(request).read()
+        #time.sleep(0.4)
     except urllib2.URLError as e:
         print('Download error : ', e.reason)
         html = None
         if num_retry > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
-                return download(url, headers, num_retry-1)
+                html = Download(url, user_agent, proxy, num_retry-1)
+                #return download(url, headers, num_retry-1)
     return html
 
 
@@ -27,8 +33,8 @@ def crawl_sitemap(url):
         print link
 
 #print(download("http://www.baidu.com"))
-#download("http://www.baidu.com")
+download("http://www.baidu.com")
 #download("http://www.meetup.com")
 #download("http://httpstat.us/500", 5)
-if __name__ == "__main__":
-    print download('http://example.webscraping.com/robot.txt')
+#if __name__ == "__main__":
+#    print download('http://example.webscraping.com/robot.txt')
